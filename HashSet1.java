7f931656b78bb4df5c0c2f46be0cacc289e9409f
koreprasad1 <102148473+koreprasad1@users.noreply.github.com>
2022-03-28 17:19:52 +0530

    Add files via upload

---
+++ HashSet1.java
@@ -0,0 +1,24 @@
+package CollectionTest;
+
+import java.util.Iterator;
+
+import java.util. *;
+public class HashSet1 {
+		public static void main(String args[]) {
+		
+			HashSet<String> set=new HashSet ();
+
+		set.add("One");
+		set.add ("Two");
+		set.add("Three");
+		set.add("Four");
+		set.add("Five");
+		Iterator<String> i=set.iterator();		
+	
+		while(i.hasNext () )
+		{
+			System.out.println(i.next());
+		}
+	}
+
+}
